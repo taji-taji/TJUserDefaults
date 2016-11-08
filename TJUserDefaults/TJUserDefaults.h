e299891c8ab1d3c7a4231b4a250e6fc7934511e0
Yutaka Tajika <y.tajika529@gmail.com>
2016-11-08 22:40:07 +0900

    Initial Commit

---
+++ TJUserDefaults/TJUserDefaults.h
@@ -0,0 +1,19 @@
+//
+//  TJUserDefaults.h
+//  TJUserDefaults
+//
+//  Created by tajika on 2016/11/08.
+//  Copyright © 2016年 Tajika. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TJUserDefaults.
+FOUNDATION_EXPORT double TJUserDefaultsVersionNumber;
+
+//! Project version string for TJUserDefaults.
+FOUNDATION_EXPORT const unsigned char TJUserDefaultsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TJUserDefaults/PublicHeader.h>
+
+
